95cb47ec7a3ed210aef81f73da0b2ed856a768fd
GTgyani206 <gyanendrathakur4135stella@gmail.com>
2024-04-03 16:15:50 +0530

    Fixed a bug in the data initialization and added more code for the backened added cloud storage too

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+
+const listingsRoutes = require("./routes/listings");
+const reviewsRoutes = require("./routes/reviews");
+const userRoutes = require("./routes/users");
+
+const Mongo_URL = "mongodb://127.0.0.1:27017/codsoft";
+
+main()
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(Mongo_URL);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+  secret: "supersecret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
+});
+
+//Listing Routes
+app.use("/listings", listingRouter);
+//Review Routes
+app.use("/listings/:id/reviews", reviewRouter);
+//User Routes
+app.use("/", userRouter);
+
+//eror handeling
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found!"));
+});
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something Went Wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+});
+
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
